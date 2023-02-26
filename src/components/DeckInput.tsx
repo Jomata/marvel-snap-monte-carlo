3f79ed035a442b338abc86e5e61a31817cd6f0fb
Qbe <jose.jtp@gmail.com>
2023-02-26 13:10:57 +0900

    Tweaking deck input UI

--- src/components/DeckInput.tsx
+++ src/components/DeckInput.tsx
@@ -1,10 +1,7 @@
 import {
-MultiSelect,
-MultiSelectProps,
-Box,
-CloseButton,
-MultiSelectValueProps,
+Select,
 } from "@mantine/core";
+import { useState } from "react";
 import { CardName, CARD_NAMES } from "../data/cards";
 
 export type DeckInputProps = {
@@ -16,56 +13,27 @@ export default function DeckInput({
     deck,
     onDeckChange,
 }:DeckInputProps) {
-    return (<MultiSelect
-        data={CARD_NAMES}
-        maxSelectedValues={12}
-        valueComponent={Value}
-        // itemComponent={Item}
-        searchable
-        // defaultValue={["US", "FI"]}
-        placeholder="Choose cards for your deck"
-        onChange={v => onDeckChange(v.map(c => c as CardName))}
-        value={deck}
-        wrapperProps={{"style":{"height":"12em"}}}
-        initiallyOpened
-        
-    />)
-}
+    const [focusedInput, setFocusedInput] = useState<number | undefined>(undefined)
+    const handleSelectChange = (index: number, value: CardName) => {
+      //TODO: Focus next input after select
+      const newValues = [...deck];
+      newValues[index] = value;
+      setFocusedInput(index+1);
+      onDeckChange(newValues);
+    };
 
-function Value({
-    value,
-    label,
-    onRemove,
-    classNames,
-    ...others
-  }: MultiSelectValueProps & { value: string }) {
-    return (
-        <div style={{"width":"100%"}} {...others}>
-        <Box
-          sx={(theme) => ({
-            display: "flex",
-            cursor: "default",
-            alignItems: "center",
-            backgroundColor:
-              theme.colorScheme === "dark" ? theme.colors.dark[7] : theme.white,
-            // border: `1px solid ${
-            //   theme.colorScheme === "dark"
-            //     ? theme.colors.dark[7]
-            //     : theme.colors.gray[4]
-            // }`,
-            // paddingLeft: 10,
-            // borderRadius: 4,
-          })}
-        >
-          <CloseButton
-            onMouseDown={onRemove}
-            variant="transparent"
-            size={22}
-            iconSize={14}
-            tabIndex={-1}
-          />
-          <Box sx={{ lineHeight: 1, fontSize: 12 }}>{label}</Box>
-        </Box>
-      </div>
-    );
-  }
+    return (<>
+      {Array(12).fill('').map((_, i) => (<Select
+        key={i}
+        data={CARD_NAMES.map(c => ({ value: c, label: c, disabled: deck.some(dc => dc === c && c !== deck.at(i)) }))}
+        searchable
+        placeholder={`Choose card #${i} for your deck`}
+        onChange={v => handleSelectChange(i, v as CardName)}
+        value={deck.at(i)}
+        disabled={deck.length < i}
+        initiallyOpened={i === focusedInput}
+        autoFocus={i === focusedInput}
+        size="xs"
+    />))}
+    </>)
+}
